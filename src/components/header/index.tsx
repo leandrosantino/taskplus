b3043f9e55556441e527c9f61cf681c1817689f7
leandrosantino <leandrosantino2013@gmail.com>
2023-04-25 18:53:13 -0300

    Added Sass Compiler

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,5 +1,5 @@
 import Link from 'next/link'
-import styles from './style.module.css'
+import styles from './style.module.scss'
 
 
 export default function Header() {
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,5 @@
 import Head from 'next/head'
-import styles from '@/styles/home.module.css'
+import styles from '@/styles/home.module.scss'
 import Image from 'next/image'
 
 import heroImg from '@public/assets/hero.png'
