bc44a942267ae3a205fa75509993eddf1c105ca4
leandrosantino <leandrosantino2013@gmail.com>
2023-06-23 12:00:44 -0300

    created admin routes

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -2,6 +2,8 @@ import Head from 'next/head'
 import styles from './styles.module.scss'
 import { FaShare, FaTrash } from 'react-icons/fa'
 import { Textarea } from '@/components/textarea'
+import { GetServerSideProps } from 'next'
+import { getSession } from 'next-auth/react'
 
 export default function Dashboard() {
   return (
@@ -55,5 +57,20 @@ export default function Dashboard() {
 
     </div>
   )
+}
+
+export const getServerSideProps: GetServerSideProps = async ({ req }) => {
+  const session = await getSession({ req })
+  if (!session?.user) {
+    return {
+      redirect: {
+        destination: '/',
+        permanent: false
+      }
+    }
+  }
+  return {
+    props: {}
+  }
 }
 
